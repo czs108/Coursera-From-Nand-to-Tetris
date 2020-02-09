2505ac41148e6489fd75526a68c4b3c199112754
Chenzs108 <chenzs108@outlook.com>
2020-02-09 19:15:30 +0800

    Feat: Add `Parser`

---
+++ Project-7 Virtual Machine I - Stack Arithmetic/Source Files/CmdType.cs
@@ -0,0 +1,16 @@
+namespace Project7
+{
+    public enum CmdType
+    {
+        Arithmetic,
+        Push,
+        Pop,
+        Label,
+        Goto,
+        If,
+        Function,
+        Return,
+        Call,
+        Whitespace
+    }
+}
