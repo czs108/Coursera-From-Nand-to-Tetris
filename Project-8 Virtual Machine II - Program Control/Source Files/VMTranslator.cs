afdfae812e71f8e39e3f3b93157a8aae420250c2
Chenzs108 <chenzs108@outlook.com>
2020-02-10 10:55:11 +0800

    Feat: Update the VMTranslator

--- Project-8 Virtual Machine II - Program Control/Source Files/VMTranslator.cs
+++ Project-8 Virtual Machine II - Program Control/Source Files/VMTranslator.cs
@@ -6,6 +6,8 @@ namespace Project8
 {
     public class VMTranslator
     {
+        private const string SYSTEM_INIT = "Sys.vm";
+
         public void TranslateFile(string inputFile, string outputFile)
         {
             Debug.Assert(!String.IsNullOrWhiteSpace(inputFile));
@@ -17,6 +19,25 @@ namespace Project8
             }
         }
 
+        public void TranslateDirectory(string inputDir, string outputFile)
+        {
+            Debug.Assert(!String.IsNullOrWhiteSpace(inputDir));
+            Debug.Assert(!String.IsNullOrWhiteSpace(outputFile));
+
+            using (CodeWriter writer = new CodeWriter(outputFile))
+            {
+                if (File.Exists(Path.Combine(inputDir, SYSTEM_INIT)))
+                {
+                    writer.WriteInit();
+                }
+
+                foreach (string inputFile in Directory.GetFiles(inputDir, "*.vm"))
+                {
+                    TranslateFile(inputFile, writer);
+                }
+            }
+        }
+
         private void TranslateFile(string inputFile, CodeWriter writer)
         {
             Debug.Assert(!String.IsNullOrWhiteSpace(inputFile));
@@ -24,6 +45,8 @@ namespace Project8
 
             using (Parser parser = new Parser(inputFile))
             {
+                writer.SetFileName(inputFile);
+
                 while (parser.HasMoreCommands())
                 {
                     parser.Advance();
@@ -44,9 +67,38 @@ namespace Project8
                             writer.WritePushPop("pop", parser.Arg1(), parser.Arg2());
                             break;
                         }
+                        case CmdType.Label:
+                        {
+                            writer.WriteLabel(parser.Arg1());
+                            break;
+                        }
+                        case CmdType.Goto:
+                        {
+                            writer.WriteGoto(parser.Arg1());
+                            break;
+                        }
+                        case CmdType.If:
+                        {
+                            writer.WriteIf(parser.Arg1());
+                            break;
+                        }
+                        case CmdType.Function:
+                        {
+                            writer.WriteFunction(parser.Arg1(), parser.Arg2());
+                            break;
+                        }
+                        case CmdType.Call:
+                        {
+                            writer.WriteCall(parser.Arg1(), parser.Arg2());
+                            break;
+                        }
+                        case CmdType.Return:
+                        {
+                            writer.WriteReturn();
+                            break;
+                        }
                         default:
                         {
-                            // TODO...
                             break;
                         }
                     }
@@ -54,27 +106,45 @@ namespace Project8
             }
         }
 
-        private static string GetOutputName(string inputName)
+        private static string GetOutputName(string inputName, out bool isDirectory)
         {
             Debug.Assert(!String.IsNullOrWhiteSpace(inputName));
 
-            return Path.Combine(Path.GetDirectoryName(inputName),
-                $"{Path.GetFileNameWithoutExtension(inputName)}.asm");
+            if (String.IsNullOrEmpty(Path.GetExtension(inputName)))
+            {
+                isDirectory = true;
+                return Path.Combine(inputName,
+                    $"{Path.GetFileNameWithoutExtension(inputName)}.asm");
+            }
+            else
+            {
+                isDirectory = false;
+                return Path.Combine(Path.GetDirectoryName(inputName),
+                    $"{Path.GetFileNameWithoutExtension(inputName)}.asm");
+            }
         }
 
         public static void Main(string[] args)
         {
             if (args.Length < 1)
             {
-                Console.WriteLine(" [!] ERROR: There is no input file.");
+                Console.WriteLine(" [!] ERROR: There is no input file or directory.");
                 return;
             }
 
             try
             {
                 string input = args[0];
-                string output = GetOutputName(input);
-                new VMTranslator().TranslateFile(input, output);
+                string output = GetOutputName(input, out bool isDirectory);
+                if (isDirectory)
+                {
+                    new VMTranslator().TranslateDirectory(input, output);
+                }
+                else
+                {
+                    new VMTranslator().TranslateFile(input, output);
+                }
+
                 Console.WriteLine(" [*] The translation has finished.");
             }
             catch (Exception e)
