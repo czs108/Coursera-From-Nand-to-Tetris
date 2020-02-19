4b63b2fc5940213293a27484400dcc41d01cf10f
Chenzs108 <chenzs108@outlook.com>
2020-02-19 14:31:33 +0800

    Feat: Add `Tokenizer`

---
+++ Project-10 Compiler I - Syntax Analysis/Source Files/KeywordType.cs
@@ -0,0 +1,27 @@
+namespace Project10
+{
+    public enum KeywordType
+    {
+        Class,
+        Method,
+        Function,
+        Constructor,
+        Int,
+        Boolean,
+        Char,
+        Void,
+        Var,
+        Static,
+        Field,
+        Let,
+        Do,
+        If,
+        Else,
+        While,
+        Return,
+        True,
+        False,
+        Null,
+        This
+    }
+}
